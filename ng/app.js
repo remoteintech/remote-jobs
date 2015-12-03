72f1d782b6f092a8c92a63b8a9a731338d2eac32
Leo Botinelly <lmbs001@bucknell.edu>
2015-12-02 19:39:32 -0500

    Initial deploy

---
+++ ng/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var app = angular.module('remote-jobs', ['yaru22.md']);
+
+app.controller('menuController', function ($scope, $http, $filter) {
+
+    $scope.search = {};
+
+
+    $http.get('https://rawgit.com/jessicard/remote-jobs/master/README.md').then(function (data) {
+        var dataSection = false;
+        var res = [];
+        var preData = data.data.split("\n");
+        preData.forEach(function (entry) {
+            if (dataSection) {
+                var e = entry.split('|');
+
+                var rName = e[0];
+                var cName = rName;
+
+                var mdRef = null;
+                if (rName.indexOf('[') != -1) {
+                    cName = rName.split('[')[1].split(']')[0];
+
+                    mdRef = cName;
+
+                    if (rName.indexOf('(') != -1) {
+                        mdRef = rName.split('(')[1].split(')')[0];
+                    }
+
+                }
+                res.push({ company: cName, mdRef: mdRef, url: e[1], place: e[2] });
+            }
+            else
+                if (entry.indexOf('---') != -1) dataSection = true;
+        });
+        $scope.data = res;
+    }, function (data) {
+        //Failed to load: Decide what to do later.
+    });
+
+    $scope.loadMd = function (ref) {
+
+        console.log(ref);
+
+        $http.get(ref.mdRef )
+            .then(function (data) { $scope.mdContent = data.data }, function (data) { $scope.mdContent = data });
+
+    }
+});
---
+++ ng/libs/markdown.js
@@ -0,0 +1,36 @@
+/*
+ * angular-markdown-directive v0.3.1
+ * (c) 2013-2014 Brian Ford http://briantford.com
+ * License: MIT
+ */
+
+'use strict';
+
+angular.module('btford.markdown', ['ngSanitize']).
+  provider('markdownConverter', function () {
+    var opts = {};
+    return {
+      config: function (newOpts) {
+        opts = newOpts;
+      },
+      $get: function () {
+        return new Showdown.converter(opts);
+      }
+    };
+  }).
+  directive('btfMarkdown', ['$sanitize', 'markdownConverter', function ($sanitize, markdownConverter) {
+    return {
+      restrict: 'AE',
+      link: function (scope, element, attrs) {
+        if (attrs.btfMarkdown) {
+          scope.$watch(attrs.btfMarkdown, function (newVal) {
+            var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
+            element.html(html);
+          });
+        } else {
+          var html = $sanitize(markdownConverter.makeHtml(element.text()));
+          element.html(html);
+        }
+      }
+    };
+  }]);
