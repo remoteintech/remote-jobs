b39c47fc6cd1338fb04abf454cafb0c0b0bfcc7f
James Nylen <jnylen@gmail.com>
2018-04-21 22:51:21 -0500

    Remove temporary code to generate minimal profiles

--- bin/validate.js
+++ bin/validate.js
@@ -125,27 +125,6 @@ $( 'tr' ).each( ( i, tr ) => {
 		if ( match ) {
 			entry.linkedFilename = match[ 1 ];
 			if ( profileFilenames.indexOf( entry.linkedFilename ) === -1 ) {
-				const profileFilename = companyNameToProfileFilename( entry.name ) + '.md';
-				fs.writeFileSync(
-					path.join( profilesPath, profileFilename ),
-					require( 'util' ).format(
-						[
-							'# %s',
-							'',
-							'## Company blurb',
-							'',
-							"\u26a0 We don't have much information about this company yet!",
-							'',
-							"If you know something we don't, help us fill it in!  Here's how:",
-							'',
-							'- Read our [Contributing Guidelines](https://github.com/remoteintech/remote-jobs/blob/master/CONTRIBUTING.md)',
-							'- Have a look at our [example company profile](https://github.com/remoteintech/remote-jobs/blob/master/company-profiles/example.md)',
-							'- Follow the structure of the example profile and [send us a pull request with your changes to this file!](https://github.com/remoteintech/remote-jobs/edit/master/company-profiles/%s)',
-						].join( '\n' ) + '\n',
-						entry.name,
-						profileFilename
-					)
-				);
 				readmeError(
 					'Broken link to company "%s": "%s"',
 					entry.name,
