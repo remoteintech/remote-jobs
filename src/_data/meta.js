9673eeae17e899996e26ee4625daa8a9bae92a5b
Doug Aitken <dougaitken@users.noreply.github.com>
2025-10-15 22:14:54 +0100

    Update project metadata and remove legacy files
    Removed Netlify and Vercel config files, builtwith.json, and various unused images. Updated metadata, personal info, and external link CSS to reflect new project ownership and domain. Simplified readme and commented out previous author details.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -8,17 +8,17 @@ export const locale = 'en_EN';
 export const lang = 'en';
 export const skipContent = 'Skip to content';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
-  avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
+//  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+//  avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
+//  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
+//  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
+//  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
 };
 export const creator = {
-  name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
-  email: 'hola@lenesaile.com',
-  website: 'https://www.lenesaile.com',
-  social: 'https://front-end.social/@lene'
+  name: 'LDoug Aitken',
+  email: 'doug@dougaitken.co.uk',
+//  website: 'https://www.lenesaile.com',
+//  social: 'https://front-end.social/@lene'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
 export const themeColor = '#dd4462'; // used in manifest, for example primary color value
